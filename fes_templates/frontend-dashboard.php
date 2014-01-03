6fb2e6f249ab92a3d3e923751fadfb60d869f0bf
Sunny Ratilal <sunnyratilal5@gmail.com>
2014-01-03 16:49:27 +0000

    Change textdomain to lattice

--- fes_templates/frontend-dashboard.php
+++ fes_templates/frontend-dashboard.php
@@ -5,7 +5,7 @@
 			$user = new WP_User(get_current_user_id());
 			$user = get_userdata(get_current_user_id());
 			$vendor_url = add_query_arg( 'vendor', $user->user_nicename, get_permalink( EDD_FES()->fes_options->get_option( 'vendor-page') ) );
-			printf( __(' Your store url is: %s', 'edd_fes' ), '<a href="' . $vendor_url . '">' . $vendor_url . '</a>' );
+			printf( __(' Your store url is: %s', 'lattice' ), '<a href="' . $vendor_url . '">' . $vendor_url . '</a>' );
 		?>
 		</p>
 	</div>
