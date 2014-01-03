32f5bc5a926a46a6a30ce719ad8f2b1279c3b79a
Sunny Ratilal <sunnyratilal5@gmail.com>
2014-01-03 17:34:20 +0000

    Add in DocBlock. Move global $post down. Add a message if comments are closed

--- comments.php
+++ comments.php
@@ -16,17 +16,24 @@
 	} // end if
 ?>
 
-<?php global $post; ?>
-
-<?php if ( post_password_required() ) { ?>
+<?php
+/*
+ * If the current post is protected by a password and
+ * the visitor has not yet entered the password we will
+ * return early without loading the comments.
+ */
+if ( post_password_required() ) {
+?>
 	<div id="comments">
 		<h3 class="nopassword"><?php _e( 'This post is password protected. Enter the password to view comments.', 'lattice' ); ?></h3>
 	</div><!-- #comments -->
 	<?php return; ?>
 <?php } // end if ?>
 
+<?php global $post; ?>
 
 <?php if ( have_comments() ) { ?>
+
 	<div id="comments" class="clearfix">
 		<h3><?php lattice_comments_title(); ?></h3>
 		<div id="comments-list">
@@ -71,6 +78,12 @@
 				<p><?php _e( 'Be the first to start the conversation.', 'lattice' ); ?></p>
 
 			<?php } // end if ?>
+		</div><!-- /#comments -->
+
+	<?php } elseif ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
+
+		<div id="comments" class="clearfix">
+			<p class="no-comments"><?php _e( 'Comments are closed.', 'lattice' ); ?></p>
 		</div><!-- /#comments -->
 
 	<?php } // end if ?>
