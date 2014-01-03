a3b699663e9172718b106c417d6145486a31eb4e
Sunny Ratilal <sunnyratilal5@gmail.com>
2014-01-03 17:38:54 +0000

    Brace formatting. Code formatting & clean up.

--- content.php
+++ content.php
@@ -14,59 +14,62 @@
 	<?php
 	lattice_post_thumbnail();
 
-	if ( ! is_single() ) :
+	if ( ! is_single() ) {
 		the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-	endif;
+	} // end if
 	?>
 
-	<?php if ( ! ( is_single() || is_singular() ) ) : ?>
+	<?php if ( ! ( is_single() || is_singular() ) ) { ?>
 		<div class="entry-meta">
 			<?php
-			if ( 'post' == get_post_type() )
+			if ( 'post' == get_post_type() ) {
 				lattice_post_date();
+			} // end if
 
-			if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+			if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			?>
 				<span class="comments-number"><?php lattice_comments_popup_link(); ?></span>
 			<?php
-			endif;
+			} // end if
 
-			if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) :
+			if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) ) {
 			?>
 				<span class="category-links"><?php echo get_the_category_list( ', ' ); ?></span>
 			<?php
-			endif;
+			} // end if
 
 			edit_post_link( __( 'Edit', 'lattice' ), '<span class="edit-link">', '</span>' );
 			?>
 		</div><!-- /.entry-meta -->
-	<?php endif; ?>
+	<?php } // end if ?>
 
-	<?php if ( is_search() ) : ?>
+	<?php if ( is_search() ) { ?>
 
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><!-- /.entry-summary -->
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><!-- /.entry-summary -->
 
-	<?php else : ?>
+	<?php } else { ?>
 
-	<div class="entry-content">
-		<?php
-			the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'lattice' ) );
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
-				'after'       => '</div>',
-				'link_before' => '<span>',
-				'link_after'  => '</span>',
-			) );
-		?>
-	</div><!-- /.entry-content -->
+		<div class="entry-content">
+			<?php
+				the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'lattice' ) );
+				wp_link_pages( array(
+					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfourteen' ) . '</span>',
+					'after'       => '</div>',
+					'link_before' => '<span>',
+					'link_after'  => '</span>',
+				) );
+			?>
+		</div><!-- /.entry-content -->
 
-	<?php endif; ?>
+	<?php } // end if ?>
 
-	<?php if ( get_the_tags() ) : ?>
-	<footer class="entry-footer">
-		<?php the_tags( '<i class="fa fa-tags"></i> <span class="tags">', ', ', '</span>' ); ?>
-	</footer>
-	<?php endif; ?>
+	<?php if ( get_the_tags() ) { ?>
+
+		<footer class="entry-footer">
+			<?php the_tags( '<i class="fa fa-tags"></i> <span class="tags">', ', ', '</span>' ); ?>
+		</footer>
+
+	<?php } // end if ?>
 </article><!-- #post-<?php the_ID(); ?> -->
