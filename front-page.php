d0fc9e138e34f1d8ebe99748050ecfb41c1db056
Sunny Ratilal <sunnyratilal5@gmail.com>
2014-01-05 17:10:04 +0000

    DocBlock

--- front-page.php
+++ front-page.php
@@ -2,11 +2,11 @@
 /**
  * The homepage template file.
  *
- * @package   Lattice
- * @version   1.0
- * @since     1.0
- * @author	  Sunny Ratilal
- * @copyright Copyright (c) 2013, Sunny Ratilal.
+ * @link http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Lattice
+ * @since	1.0
+ * @version	1.0
  */
 ?>
 <?php get_header(); ?>
