25961f3a76b96a46347eccf85332331601f5bde3
Sunny Ratilal <sunnyratilal@users.noreply.github.com>
2014-02-10 07:23:59 +0000

    Version bump to 1.1

--- functions.php
+++ functions.php
@@ -32,7 +32,7 @@
  *
  * @since   1.0
  */
-define( 'LATTICE_THEME_VERSION', '1.0' );
+define( 'LATTICE_THEME_VERSION', '1.1' );
 
 /* ----------------------------------------------------------- *
  * 1. Theme Setup
